581c180d1f669ee86271aa5c46194273be3c8271
VIVEK <99870677+Roninopp@users.noreply.github.com>
2022-12-18 13:41:21 +0530

    Update sample_config.py

--- Sibyl_System/sample_config.py
+++ Sibyl_System/sample_config.py
@@ -1,11 +1,10 @@
 from typing import Union, Optional
 
-API_ID: int = 12345
-API_HASH: str = "XYZ"
-STRING_SESSION: str = "XYZ:XYZ:XYZ...."
-Sibyl_logs: Union[str, int] = 123456789
-Sibyl_approved_logs: Union[str, int] = 123456789
-GBAN_MSG_LOGS: Optional[Union[str, int]] = None
-BOT_TOKEN: str = "123456:shsiajskzkxhxj"
-
-# Remove this line one you have read and understood the explanation of things in https://github.com/AnimeKaizoku/SibylSystem/blob/master/README.md
+API_ID: int = 7217645
+API_HASH: str = "78ba6352dd5cdc166fdef5aa84ba7c67"
+STRING_SESSION: str = "1BVtsOKMBu0wCUPfFNnMRtIB-920Wuk1Uu0Ujw3r_ilouGQRJrjKoJ4cavTroZpKAmZ2k3U042CIW3uZbZsKN3H8aQQckQ_E3XyONuj6tJhaYnOECF2ATc-bvjgBTcwPkuXTz3tWe4uSiSqQQ6VWRo3MWzLU8Pscgk0qE2J75gQZvWzrMzS0K8Q0HZdlz0QVoDzF6dCEARguEIbllyD4GUjp6VbIqBdSDZ3MKA99beEyLNSfLdeWJZNGlO6cj21WNSJGg2aR49krCg8TrRPiiDOYNpgBmOncitnQUrod-yj2a4oCyVWoYzfISE--9Nh5u_jzmxam_4Ghl_WiIP3iXkSdHZkvRAqE="
+Sibyl_logs: Union[str, int] = -1001644439158
+Sibyl_approved_logs: Union[str, int] = -1001644439158
+GBAN_MSG_LOGS: Optional[Union[str, int]] = -1001564289796
+BOT_TOKEN: str = "5402826348:AAGjSCrB8iAesNZaobMJgRpbK2yX_IhvHt0"
+MONGO_DB_URL: str = "mongodb+srv://DARKAMAN:DARKAMAN@cluster0.snqhn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
